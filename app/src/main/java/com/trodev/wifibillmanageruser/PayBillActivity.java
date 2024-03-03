2ba899fe7f4629d77aef252fcee22ed8bcea3658
zobayerdev <zobayer.dev@gmail.com>
2024-03-03 17:32:59 +0600

    update billing api

--- app/src/main/java/com/trodev/wifibillmanageruser/PayBillActivity.java
+++ app/src/main/java/com/trodev/wifibillmanageruser/PayBillActivity.java
@@ -1,12 +1,14 @@
 package com.trodev.wifibillmanageruser;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,11 +28,13 @@ import com.sslwireless.sslcommerzlibrary.model.initializer.SSLCommerzInitializat
 import com.trodev.wifibillmanageruser.models.BillModels;
 import com.trodev.wifibillmanageruser.models.User;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PayBillActivity extends AppCompatActivity {
-    private static final String API_KEY = "4be324433504126ddd49662efcf7f111740895b6";
+    private static final String API_KEY = "5078f2cffe40017a8e7648b5ec98c763035f44b8";
     private static final String CHECKOUT_URL = "https://payment.trodev.com/api/checkout-v2";
     private static final String VERIFY_PAYMENT_URL = "https://payment.trodev.com/api/verify-payment";
     private static final String REDIRECT_URL = "https://www.trodev.com";
@@ -58,7 +62,7 @@ public class PayBillActivity extends AppCompatActivity {
     private String storedMetaValue3;
 
     private MaterialButton payBtn;
-    TextInputEditText nameEt, emailEt, amountEt;
+    TextInputEditText nameEt, emailEt, amountEt, uidEt, mobileET;
     LinearLayout webLayout, uiLayout;
     WebView payWebView;
     TextView resultTv;
@@ -67,6 +71,9 @@ public class PayBillActivity extends AppCompatActivity {
     private FirebaseUser user;
     private String userID;
     BillModels userProfile;
+    AutoCompleteTextView autoCompleteTextView;
+    String b_name, b_email, b_price, b_mobile, b_month, b_packages, b_user_id;
+    String biller_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +87,8 @@ public class PayBillActivity extends AppCompatActivity {
         nameEt = findViewById(R.id.nameEt);
         emailEt = findViewById(R.id.emailEt);
         amountEt = findViewById(R.id.amountEt);
+        mobileET = findViewById(R.id.mobileET);
+        uidEt = findViewById(R.id.uidEt);
 
         /*web view & ui layout*/
         webLayout = findViewById(R.id.webLayout);
@@ -89,6 +98,23 @@ public class PayBillActivity extends AppCompatActivity {
         /*button*/
         payBtn = findViewById(R.id.payBtn);
 
+        /*Package adapter view*/
+
+        /* String[] type = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, R.layout.drop_down_list, type
+        );
+
+        autoCompleteTextView = findViewById(R.id.filled_exposed);
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(PayBillActivity.this, autoCompleteTextView.getText().toString() + " select", Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
         /*####################################################################################################################################*/
         /*####################################################################################################################################*/
         /*get admin info from admin user database*/
@@ -109,10 +135,15 @@ public class PayBillActivity extends AppCompatActivity {
                     String u_name = userProfile.uname;
                     String u_email = userProfile.email;
                     String u_price = userProfile.prices;
+                    String u_id = userProfile.user_token;
+                    String u_mobile = userProfile.num;
 
+                    // uidEt.setText(u_id);
                     nameEt.setText(u_name);
                     emailEt.setText(u_email);
                     amountEt.setText(u_price);
+                    uidEt.setText(u_id);
+                    mobileET.setText(u_mobile);
 
                 }
             }
@@ -193,6 +224,7 @@ public class PayBillActivity extends AppCompatActivity {
                                     // Handle payment completed case
                                     uiLayout.setVisibility(View.VISIBLE);
                                     webLayout.setVisibility(View.GONE);
+                                    save_db();
                                     resultTv.setText("Payment Complete" + "\n" + "Name:  " + storedFullName + "\n" + "Amount: " + storedAmount);
 
                                 } else if ("PENDING".equals(status)) {
@@ -220,4 +252,62 @@ public class PayBillActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void save_db() {
+
+        b_name = nameEt.getText().toString().trim();
+        b_email = emailEt.getText().toString().trim();
+        b_price = amountEt.getText().toString().trim();
+        b_mobile = mobileET.getText().toString().trim();
+        b_user_id = uidEt.getText().toString().trim();
+
+        // b_packages = packagesET.getText().toString().trim();
+        // b_user_id = user_token_Et.getText().toString().trim();
+        b_month = autoCompleteTextView.getText().toString().trim();
+
+        reference = FirebaseDatabase.getInstance().getReference("user_bill");
+
+        String transaction_id = storedTransactionId;
+
+        if (!(!b_name.isEmpty() || !b_email.isEmpty() || !b_price.isEmpty() || !b_mobile.isEmpty() || !b_packages.isEmpty() || !b_user_id.isEmpty())) {
+            Toast.makeText(this, "please fill all field", Toast.LENGTH_SHORT).show();
+        } else if (b_packages.isEmpty()) {
+            // packagesET.setError("Please select the month");
+        } else {
+
+            // Its a time and date code show our user and admin
+            // amra date and time init kore database e save korchi
+            Calendar calForYear = Calendar.getInstance();
+            SimpleDateFormat currentYear = new SimpleDateFormat("yyyy");
+            String year = currentYear.format(calForYear.getTime());
+
+            Calendar calForDate = Calendar.getInstance();
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd/MM/yyyy");
+            String date = currentDate.format(calForDate.getTime());
+
+            Calendar calForTime = Calendar.getInstance();
+            SimpleDateFormat currentTime = new SimpleDateFormat("hh:mm a");
+            String time = currentTime.format(calForTime.getTime());
+
+
+            String Key = reference.push().getKey();
+
+            if (Key != null) {
+
+                BillModels billModels = new BillModels(b_name, b_user_id, b_packages, b_mobile, b_month, b_email, b_price, date, time, year, transaction_id, FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                // these data save on new uid and also user id
+                reference.child(Key).setValue(billModels);
+
+                // these data save on user id
+                // reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(billModels);
+
+                Toast.makeText(this, "save successful", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "save un-successful", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
 }
